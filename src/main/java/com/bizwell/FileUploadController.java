88115ac96d7fbbed8f05fb447434958f7010a586
liujian <liujian3>
2018-04-27 15:47:15 +0800

    增加上传文件的FileUploadController

---
+++ src/main/java/com/bizwell/FileUploadController.java
@@ -0,0 +1,56 @@
+package com.bizwell;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.List;
+
+/**
+ * 文件上传的Controller
+ * Created by liujian on 2018/4/27.
+ */
+@Controller
+public class FileUploadController {
+    @RequestMapping(value = "/upload", method = RequestMethod.GET)
+    public String upload() {
+        return "/fileupload";
+    }
+
+    
+    /**
+     * 文件上传具体实现方法（单文件上传）
+     *
+     * @param file
+     * @return
+     */
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                BufferedOutputStream out = new BufferedOutputStream(
+                        new FileOutputStream(new File(file.getOriginalFilename())));
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return "上传失败," + e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "上传失败," + e.getMessage();
+            }
+            return "上传成功";
+        } else {
+            return "上传失败，因为文件是空的.";
+        }
+    }
+  
+}
