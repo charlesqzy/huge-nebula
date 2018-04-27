2d0182f0608761ce4d9b8e1736f5c42c824578fc
queziyang <queziyang@niwodai.net>
2018-04-27 10:16:47 +0800

    初始化项目

---
+++ src/main/java/com/bizwell/HugeNebulaApplication.java
@@ -0,0 +1,12 @@
+package com.bizwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugeNebulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HugeNebulaApplication.class, args);
+	}
+}
